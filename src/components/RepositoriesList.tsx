a0f9866e12119c0a8458f815ceab2439d1507a97
aliatayee <aliaqaatayee@gmail.com>
2022-09-18 14:52:03 +0430

    Updated layout designs

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,28 +1,62 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const RepositoriesList: React.FC = () => {
     const [term, setTerm] = useState('');
-    const { searchRepositories} = useActions();
-    const {data, error, loading} = useTypedSelector((state) => state.repositories)
-    console.log(data);
-const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    searchRepositories(term);
-}
+    const { searchRepositories } = useActions();
+    const inputRef = useRef<HTMLInputElement | null>(null)
+    const { data, error, loading } = useTypedSelector((state) => state.repositories)
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [])
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        searchRepositories(term);
+    }
     return (
         <div>
-        <form action="" onSubmit={onSubmit}>
-            <input type="text" value={term} onChange={e => setTerm(e.target.value)} />
-            <button>Search</button>
-        </form>
-        {error && <h3>{error}</h3>}
-        {loading && <h3>Loading...</h3>}
-        <ul>
-        {!error && !loading && data.map((name)=> <li key={name}>{name}</li>)}
-        </ul>
+            <section className="text-gray-400 body-font bg-gray-900">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+                        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Grab your most desired package!</h1>
+                        <p className="lg:w-1/2 w-full leading-relaxed text-opacity-80">Easily find out whatever npm pack you need and toggle over many packages</p>
+                        <form onSubmit={onSubmit} className="lg:w-1/2 mt-5">
+                            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+                            <div className="relative">
+                                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                                </div>
+                                <input type="search" ref={inputRef} value={term} onChange={e => setTerm(e.target.value)} id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                            </div>
+                        </form>
+
+                        {error && <h3>{error}</h3>}
+                        {loading && <h3>Loading...</h3>}
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        {!error && !loading && data.map((name) =>
+                            <div className="xl:w-1/3 md:w-1/2 p-4 shadow-inner shadow-md">
+                                <div className="border border-gray-700 border-opacity-75 p-6 rounded-lg">
+                                    <div className="w-10 h-10 inline-flex items-center justify-center rounded-full bg-gray-800 text-red-400 mb-4">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-6 h-6" viewBox="0 0 24 24">
+                                            <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                                        </svg>
+                                    </div>
+                                    <h2 className="text-lg text-white font-medium title-font mb-2">{name}</h2>
+                                    <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
+            </section>
+
         </div>
     )
 }
